d46e7709551d985e2bfd1205dbb803a40e73ffd9
Marek Viger <marek.viger@gmail.com>
2013-10-21 17:10:01 +0200

    - better readability of code (uppercase constants, function curly bracket on new line, removed old commented code…) - fixed comments to match phpDoc syntax - unambiguous construct - default return type from api is now object, not assoc - method to change api return type

--- LastFM.php
+++ LastFM.php
@@ -3,12 +3,12 @@
 /*
  * This is minimal PHP library - it implements all the necessary
  * stuff, and ONLY that.
- * 
+ *
  * Implemented:
  * - authentication flow
  * - api calls wrapper
  * - error wrapper
- */ 
+ */
 
 if (!function_exists('curl_init')) {
     throw new Exception('LastFM needs the CURL PHP extension.');
@@ -22,7 +22,8 @@ if (!function_exists('json_decode')) {
  *
  * @author Filip Sobczak <f@digitalinvaders.pl>
  */
-class LastFMException extends Exception {
+class LastFMException extends Exception
+{
 
     /**
      * The result from the API server that represents the exception information.
@@ -32,9 +33,10 @@ class LastFMException extends Exception {
     /**
      * Make a new API Exception with the given result.
      *
-     * @param Array $result the result from the API server
+     * @param array $result the result from the API server
      */
-    public function __construct($result) {
+    public function __construct($result)
+    {
         $this->result = $result;
 
         $code = isset($result['error']) ? $result['error'] : 0;
@@ -51,30 +53,34 @@ class LastFMException extends Exception {
     /**
      * Return the associated result object returned by the API server.
      *
-     * @returns Array the result from the API server
+     * @return array the result from the API server
      */
-    public function getResult() {
+    public function getResult()
+    {
         return $this->result;
     }
 
     /**
      * To make debugging easier.
      *
-     * @returns String the string representation of the error
+     * @return string the string representation of the error
      */
-    public function __toString() {
+    public function __toString()
+    {
         $str = '';
         if ($this->code != 0) {
             $str .= $this->code . ': ';
         }
-        return $str . $this->message;
-    } 
 
+        return $str . $this->message;
+    }
 }
-    
-class LastFMInvalidSessionException extends LastFMException {
-    public function __construct($result) {
-        parent::__construct($result); 
+
+class LastFMInvalidSessionException extends LastFMException
+{
+    public function __construct($result)
+    {
+        parent::__construct($result);
     }
 }
 
@@ -83,7 +89,8 @@ class LastFMInvalidSessionException extends LastFMException {
  *
  * @author Filip Sobczak <f@digitalinvaders.pl>
  */
-class LastFM {
+class LastFM
+{
     const VERSION = '0.9';
 
     /**
@@ -111,11 +118,12 @@ class LastFM {
         'www' => 'https://www.last.fm/',
         'webservice' => 'https://ws.audioscrobbler.com/2.0/',
     );
+    protected $apiReturnAssoc = false;
 
-    const method_auth = 1;
-    const method_write = 2;
-    const method_get_auth = 3;
-    const method_unknown = 4;
+    const METHOD_AUTH = 1;
+    const METHOD_WRITE = 2;
+    const METHOD_GET_AUTH = 3;
+    const METHOD_UNKNOWN = 4;
 
     /*
      * Some methods require authentication (type auth),
@@ -124,101 +132,123 @@ class LastFM {
      * they all send api_sig
      * some methods are used to write data (type write)
      * they all send api_sig and sk, and use POST http method
-     * 
-     * All letters are small because users might use 
-     * variations of letter sizes, and we need to 
+     *
+     * All letters are small because users might use
+     * variations of letter sizes, and we need to
      * find these values fast, so strtolower is executed on method name.
      */
-    public static $METHOD_TYPE =
-            array(
-        'auth.getmobilesession' => self::method_get_auth,
-        'auth.getsession' => self::method_get_auth,
-        'auth.gettoken' => self::method_get_auth,
-        'album.addtags' => self::method_write,
-        'album.gettags' => self::method_auth,
-        'album.removetag' => self::method_write,
-        'album.share' => self::method_write,
-        'artist.addtags' => self::method_write,
-        'artist.gettags' => self::method_auth,
-        'artist.removetag' => self::method_write,
-        'artist.share' => self::method_write,
-        'artist.shout' => self::method_write,
-        'event.attend' => self::method_write,
-        'event.share' => self::method_write,
-        'event.shout' => self::method_write,
-        'library.addalbum' => self::method_write,
-        'library.addartist' => self::method_write,
-        'library.addtrack' => self::method_write,
-        'library.removealbum' => self::method_write,
-        'library.removeartist' => self::method_write,
-        'library.removescrobble' => self::method_write,
-        'library.removetrack' => self::method_write,
-        'playlist.addtrack' => self::method_write,
-        'playlist.create' => self::method_write,
-        'radio.getplaylist' => self::method_auth,
-        'radio.tune' => self::method_write,
-        'track.addtags' => self::method_write,
-        'track.ban' => self::method_write,
-        'track.gettags' => self::method_auth,
-        'track.love' => self::method_write,
-        'track.removetag' => self::method_write,
-        'track.scrobble' => self::method_write,
-        'track.share' => self::method_write,
-        'track.unban' => self::method_write,
-        'track.unlove' => self::method_write,
-        'track.updatenowplaying' => self::method_write,
-        'user.getrecentstations' => self::method_auth,
-        'user.getrecommendedartists' => self::method_auth,
-        'user.getrecommendedevents' => self::method_auth,
-        'user.shout' => self::method_write,
+    public static $METHOD_TYPE = array(
+        'auth.getmobilesession' => self::METHOD_GET_AUTH,
+        'auth.getsession' => self::METHOD_GET_AUTH,
+        'auth.gettoken' => self::METHOD_GET_AUTH,
+        'album.addtags' => self::METHOD_WRITE,
+        'album.gettags' => self::METHOD_AUTH,
+        'album.removetag' => self::METHOD_WRITE,
+        'album.share' => self::METHOD_WRITE,
+        'artist.addtags' => self::METHOD_WRITE,
+        'artist.gettags' => self::METHOD_AUTH,
+        'artist.removetag' => self::METHOD_WRITE,
+        'artist.share' => self::METHOD_WRITE,
+        'artist.shout' => self::METHOD_WRITE,
+        'event.attend' => self::METHOD_WRITE,
+        'event.share' => self::METHOD_WRITE,
+        'event.shout' => self::METHOD_WRITE,
+        'library.addalbum' => self::METHOD_WRITE,
+        'library.addartist' => self::METHOD_WRITE,
+        'library.addtrack' => self::METHOD_WRITE,
+        'library.removealbum' => self::METHOD_WRITE,
+        'library.removeartist' => self::METHOD_WRITE,
+        'library.removescrobble' => self::METHOD_WRITE,
+        'library.removetrack' => self::METHOD_WRITE,
+        'playlist.addtrack' => self::METHOD_WRITE,
+        'playlist.create' => self::METHOD_WRITE,
+        'radio.getplaylist' => self::METHOD_AUTH,
+        'radio.tune' => self::METHOD_WRITE,
+        'track.addtags' => self::METHOD_WRITE,
+        'track.ban' => self::METHOD_WRITE,
+        'track.gettags' => self::METHOD_AUTH,
+        'track.love' => self::METHOD_WRITE,
+        'track.removetag' => self::METHOD_WRITE,
+        'track.scrobble' => self::METHOD_WRITE,
+        'track.share' => self::METHOD_WRITE,
+        'track.unban' => self::METHOD_WRITE,
+        'track.unlove' => self::METHOD_WRITE,
+        'track.updatenowplaying' => self::METHOD_WRITE,
+        'user.getrecentstations' => self::METHOD_AUTH,
+        'user.getrecommendedartists' => self::METHOD_AUTH,
+        'user.getrecommendedevents' => self::METHOD_AUTH,
+        'user.shout' => self::METHOD_WRITE,
     );
 
     /**
      * Initialize LastFM application.
-     * 
-     * @param type $config configuration
+     *
+     * @param string $apiKey
+     * @param string|null $apiSecret (optional) Required for authenticated calls
+     * @param string|null $sessionKey (optional) Required for authenticated calls
      */
-    public function __construct($config) {
-//$this->setAppId($config['appId']);
-        $this->setApiKey($config['api_key']);
-        $this->setApiSecret($config['api_secret']);
-        if (isset($config['sk'])) {
-            $this->setSessionKey($config['sk']);
+    public function __construct($apiKey, $apiSecret = NULL, $sessionKey = NULL)
+    {
+        $this->setApiKey($apiKey);
+        if ($apiSecret !== NULL) {
+            $this->setApiSecret($apiSecret);
+        }
+        if ($sessionKey !== NULL) {
+            $this->setSessionKey($sessionKey);
         }
     }
 
-    public function setApiSecret($apiSecret) {
+    public function setApiSecret($apiSecret)
+    {
         $this->apiSecret = $apiSecret;
         return $this;
     }
 
-    public function getApiSecret() {
+    public function getApiSecret()
+    {
         return $this->apiSecret;
     }
 
-    public function setApiKey($apiKey) {
+    public function setApiKey($apiKey)
+    {
         $this->apiKey = $apiKey;
+
         return $this;
     }
 
-    public function getApiKey() {
+    public function getApiKey()
+    {
         return $this->apiKey;
     }
 
-    public function setSessionKey($sk) {
+    public function setSessionKey($sk)
+    {
         $this->sk = $sk;
+
         return $this;
     }
 
-    public function getSessionKey() {
+    public function getSessionKey()
+    {
         return $this->sk;
     }
 
-    private function methodType($method) {
+    public function setReturnAssoc()
+    {
+        $this->apiReturnAssoc = true;
+    }
+
+    public function setReturnObject()
+    {
+        $this->apiReturnAssoc = false;
+    }
+
+    private function methodType($method)
+    {
         if (isset(self::$METHOD_TYPE[strtolower($method)])) {
             return self::$METHOD_TYPE[strtolower($method)];
         } else {
-            return self::method_unknown;
+            return self::METHOD_UNKNOWN;
         }
     }
 
@@ -228,42 +258,56 @@ class LastFM {
      * The parameters:
      * - api_key: application api key
      *
-     * @param Array $callback override default redirect
-     * @return String the URL for the login flow
+     * @param array $callback override default redirect
+     * @return string the URL for the login flow
      */
-    public function getLoginUrl($callback=array()) {
+    public function getLoginUrl($callback=array())
+    {
         $params = array('api_key' => $this->getApiKey());
         if ($callback)
             $params['cb'] = $callback;
+
         return $this->getUrl('www', 'api/auth', $params);
     }
 
     /**
-     * @param type $token 32-char ASCII MD5 hash, gained by granting permissions
+     * @param string $token 32-char ASCII MD5 hash, gained by granting permissions
+     * @return array
      */
-    public function fetchSession($token = '') {
+    public function fetchSession($token = '')
+    {
         if (!$token) {
             if (isset($_GET['token']))
                 $token = $_GET['token'];
         }
+
         $result = $this->api('auth.getSession', array('token' => $token));
-        //print_r($result);
-        //print_r($result); print_r($result['session']['key']); exit;
         $name = $result['session']['name'];
         $sessionKey = $result['session']['key'];
         $this->setSessionKey($sessionKey);
-        
+
         return array('name' => $name, 'sk' => $sessionKey);
+    }
+
+    public function __call($name, $arguments)
+    {
+        $method = str_replace('_', '.', $name);
+        $params = isset($arguments[0]) ? $arguments[0] : array();
+
+        return $this->api($method, $params);
     }
 
     /**
      * Make an API call
      *
-     * @param Array $params method call object
-     * @return the decoded response object
-     * @throws LastFMApiException
+     * @param string $method
+     * @param array $params method call object
+     * @throws LastFMException
+     * @throws LastFMInvalidSessionException
+     * @return array the decoded response object
      */
-    public function api($method, $params = array()) {
+    public function api($method, $params = array())
+    {
         // generic application level parameters
         $params['api_key'] = $this->getApiKey();
         $params['format'] = 'json';
@@ -271,12 +315,9 @@ class LastFM {
         // required api method
         $params['method'] = $method;
 
-        if ($this->methodType($method))
-            $methodType = $this->methodType($method);
+        $methodType = $this->methodType($method);
 
-
-
-        if ($methodType == self::method_auth || $methodType == self::method_write) {
+        if ($methodType == self::METHOD_AUTH || $methodType == self::METHOD_WRITE) {
             if (!isset($params['sk'])) {
                 $params['sk'] = $this->getSessionKey();
             }
@@ -288,12 +329,12 @@ class LastFM {
                 unset($params['sk']);
         }
 
-        if ($methodType == self::method_get_auth || $methodType == self::method_write) {
+        if ($methodType == self::METHOD_GET_AUTH || $methodType == self::METHOD_WRITE) {
             $params['api_sig'] = $this->generateSignature($params);
         }
 
         $raw = $this->makeRequest(self::getUrl('webservice'), $params);
-        $result = json_decode($raw, true);
+        $result = json_decode($raw, $this->apiReturnAssoc);
 
         if (is_array($result) && isset($result['error'])) {
             if ($result['error'] == 9) {
@@ -304,18 +345,20 @@ class LastFM {
             } else
                 throw new LastFMException($result);
         }
+
         return $result;
     }
 
     /**
      * Makes an HTTP request.
      *
-     * @param String $url the URL to make the request to
-     * @param Array $params the parameters to use for the POST body
-     * @return String the response text
+     * @param string $url the URL to make the request to
+     * @param array $params the parameters to use for the POST body
+     * @throws LastFMException
+     * @return string the response text
      */
-    protected function makeRequest($url, $params) {
-
+    protected function makeRequest($url, $params)
+    {
         $ch = curl_init();
         $opts = self::$CURL_OPTS;
         $opts[CURLOPT_POSTFIELDS] = http_build_query($params, null, '&');
@@ -332,20 +375,21 @@ class LastFM {
             curl_close($ch);
             throw $e;
         }
-
         curl_close($ch);
+
         return $result;
     }
 
     /**
      * Build the URL for given domain alias, path and parameters.
      *
-     * @param $name String the name of the domain
-     * @param $path String optional path (without a leading slash)
-     * @param $params Array optional query parameters
-     * @return String the URL for the given parameters
+     * @param string $name the name of the domain
+     * @param string $path optional path (without a leading slash)
+     * @param array $params optional query parameters
+     * @return string the URL for the given parameters
      */
-    protected function getUrl($name, $path='', $params=array()) {
+    protected function getUrl($name, $path='', $params=array())
+    {
         $url = self::$DOMAIN_MAP[$name];
         if ($path) {
             if ($path[0] === '/') {
@@ -353,19 +397,22 @@ class LastFM {
             }
             $url .= $path;
         }
+
         if ($params) {
             $url .= '?' . http_build_query($params, null, '&');
         }
+
         return $url;
     }
 
     /**
      * Generate a signature for the given params and secret.
      *
-     * @param Array $params the parameters to sign
-     * @return String the generated signature
+     * @param array $params the parameters to sign
+     * @return string the generated signature
      */
-    protected function generateSignature($params) {
+    protected function generateSignature($params)
+    {
         // work with sorted data
         ksort($params);
 
@@ -376,9 +423,7 @@ class LastFM {
             $base_string .= $key . $value;
         }
         $base_string .= $this->getApiSecret();
+
         return md5(utf8_encode($base_string));
     }
-
 }
-
-?>
